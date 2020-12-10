d18dec59075293a64a7e7d30197ae8544b928f65
roadup <roadup.cc@gmail.com>
2020-12-10 23:21:42 +0800

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Child from './Child';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Context = React.createContext("234")
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+        console.log("%c Parent.constructor", "color: red; font-weight: bold")
+    }
+
+    componentDidMount() {
+        console.log("%c Parent.componentDidMount", "color: red; font-weight: bold")
+    }
+    static getDerivedStateFromProps(props, state) {
+        console.log("%c Parent.static.getDerivedStateFromProps", "color: red; font-weight: bold")
+        return state || null
+    }
+    getSnapshotBeforeUpdate() {
+        console.log("%c Parent.getSnapshotBeforeUpdate", "color: red; font-weight: bold")
+        return null
+    }
+    shouldComponentUpdate() {
+        console.log("%c Parent.shouldComponentUpdate", "color: red; font-weight: bold")
+        return true
+    }
+    componentDidUpdate() {
+        console.log("%c Parent.componentDidUpdate", "color: red; font-weight: bold")
+    }
+    componentWillUnmount() {
+        console.log("%c Parent.componentWillUnmount", "color: red; font-weight: bold")
+    }
+    render() {
+        console.log("%c Parent.render", "color: red; font-weight: bold")
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <Context.Provider value="234">
+                        <Child count={this.state.count} onUpdate={(value) => {
+                            console.group("------------ update ------------")
+                            this.setState({
+                                count: value
+                            }, () => {
+                                console.groupEnd()
+                            })
+                        }} />
+                    </Context.Provider>
+                    <button onClick={() => {
+                        this.props.onDelete()
+                    }}>Delete</button>
+                </header>
+            </div>
+        );
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import Context from "./context";
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+// console.log(React)
+class Entry extends React.Component {
+    constructor(props) {
+        super(props)
+        console.group("------------ mount ------------")
+        this.state = {
+            show: true,
+            theme: "white"
+        }
+    }
+    static contextType = Context
+    componentDidMount() {
+        console.groupEnd()
+    }
+    render() {
+        if (this.state.show) {
+            return <>
+                <span onClick={() => {
+                    this.setState({
+                        theme: "dark"
+                    })
+                }}> theme {this.context} </span>
+                <App onDelete={() => {
+                    console.group("------------ delete ------------")
+                    this.setState({
+                        show: false
+                    }, () => {
+                        console.groupEnd()
+                    })
+                }} />
+
+            </>
+        }
+        return null
+    }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Context.Provider value="123">
+            <Entry />
+        </Context.Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+// ReactDOM.render(<div>root2</div>, document.getElementById('root2'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
