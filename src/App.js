5475438b97fff39bfccd42fc92a9610ee9218c3f
roadup <roadup.cc@gmail.com>
2020-12-12 19:44:48 +0800

    fcous

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ import SubTree from "./SubTree";
 export default class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { select: "", flameGraph }
+        this.state = { select: "", root: flameGraph }
+
     }
     render() {
         return (
             <Context.Provider value={{
                 select: this.state.select,
                 update: (select) => {
-                    this.setState({ select })
+                    this.setState({ select: select === this.state.select ? '' : select })
+                },
+                setRoot: (node) => {
+                    this.setState({ root: node })
                 }
             }}>
                 <div className="App">
+                    {this.state.root === flameGraph ? null : <div className="back" onClick={() => {
+                        this.setState({ root: flameGraph })
+                    }}>返回到 ReactDOM.render</div>}
                     <div className={"flag " + (this.state.select ? 'show' : 'hide')} onClick={() => { this.setState({ select: "" }) }}>{this.state.select}</div>
                     <div className="scroll">
                         <div className="inner">
-                            <SubTree name="React Flame Graph" fns={this.state.flameGraph}></SubTree>
+                            <SubTree name="React Flame Graph" fns={this.state.root} parent={flameGraph}></SubTree>
                         </div>
                     </div>
                 </div>
--- src/SubTree.js
+++ src/SubTree.js
@@ -6,7 +6,7 @@ import "./SubTree.scss";
 
 
 function randomColor(key) {
-    return "#" + parseInt(md5(key).slice(0, 6), 36).toString(16).slice(0, 6) + "44"
+    return "#" + parseInt(md5(key).slice(0, 6), 36).toString(16).slice(0, 6) + "22"
 }
 /**
  *
@@ -15,30 +15,50 @@ function randomColor(key) {
 export default function Subtree(props = {}) {
 
     let [fold, setFold] = useState({})
+    var detail = ""
+    if (props.fns?.detail) {
+        console.log(props)
+        detail = props.fns.detail
+    }
 
     return props.fns ? <>
         {
-            Object.entries(props.fns).map(([key, fns], index) => {
+            Object.entries(props.fns).map(([key, subFns], index) => {
+                if (key === "detail") {
+                    return null
+                }
                 return <div className="sub-tree" key={key}>
                     <Context.Consumer>
                         {
-                            ({ select, update }) =>
-                                <header className={"name " + (select === key ? 'selected' : '')} style={{ backgroundColor: randomColor(key) }} onClick={() => {
-                                    update(key)
-                                }}>
-                                    <div className="fn">
-                                        {(!fns || Object.keys(fns).length === 0) ? null : <i className={"arrow " + (fold[index] ? 'fold' : 'un-fold')} onClick={() => {
-                                            setFold({
-                                                ...fold,
-                                                [index]: !fold[index]
-                                            })
-                                        }}></i>} <span>{key}</span>
+                            ({ select, update, setRoot }) =>
+                                <header className={"name " + (select === key.replace(/\d$/, "") ? 'selected' : '')} style={{
+                                    backgroundColor: randomColor(key.replace(/\d$/, ""))
+                                }} onClick={(event) => {
+                                    event.stopPropagation()
+                                    update(key.replace(/\d$/, ""))
+                                }} >
+                                    <div className="fn" title={detail} onDoubleClick={() => {
+                                        debugger
+                                        setRoot(subFns)
+                                    }}>
+                                        {
+                                            (!subFns || Object.keys(subFns).length === 0) ? null :
+                                                <i className={"arrow " + (fold[index] ? 'fold' : 'un-fold')} onClick={(event) => {
+                                                    event.stopPropagation()
+                                                    setFold({
+                                                        ...fold,
+                                                        [index]: !fold[index]
+                                                    })
+                                                }}>
+                                                </i>
+                                        }
+                                        <span className={fold[index] ? 'flod' : ''}>{key.replace(/\d$/, "")}</span>
                                     </div>
                                 </header>
                         }
                     </Context.Consumer>
-                    <div className="fns">
-                        {fold[index] ? null : <Subtree fns={fns} key={key} index={index}></Subtree>}
+                    <div className={"fns " + (key.startsWith("@") ? "vertical" : "")} style={{ borderColor: randomColor(key.replace(/\d$/, "")).slice(0, 7) }}>
+                        {fold[index] ? null : <Subtree fns={subFns} key={key} index={index} ></Subtree>}
                     </div>
                 </div>
             })
